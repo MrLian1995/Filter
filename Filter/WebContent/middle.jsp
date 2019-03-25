1ed73e90e8d2a36c7217703d9cf59d1b9101a7b5
lianyalong <lianyalong1995@163.com>
2019-03-25 16:39:27 +0800

    过滤器

---
+++ Filter/WebContent/middle.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <jsp:forward page="main.jsp"></jsp:forward>
+    <jsp:include page="main.jsp"></jsp:forward>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>middle</title>
+</head>
+<body>
+	middle.jsp
+	
+</body>
+</html>
---
+++ Filter/src/com/lyl/filter/FirstFilter.java
@@ -0,0 +1,50 @@
+package com.lyl.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class FirstFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy  FirstFilter........");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("start doFilter FirstFilter........");
+		
+		HttpServletRequest req=(HttpServletRequest)request;
+		HttpServletResponse rsp=(HttpServletResponse)response;
+		//重定向
+		rsp.sendRedirect(req.getContextPath()+"/main.jsp");
+		//请求转发
+		//req.getRequestDispatcher("main.jsp").forward(request, response);
+		//req.getRequestDispatcher("main.jsp").include(request, response);
+		//chain.doFilter(request, response);
+		System.out.println("end doFilter FirstFilter........");
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("init  FirstFilter........");
+	}
+	
+	
+	
+	
+	
+
+}
